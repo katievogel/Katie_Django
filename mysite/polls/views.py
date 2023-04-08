f3911e5cb2fb30d6b95252725e02101748209928
Katie Vogel <kvog983@gmail.com>
2023-04-08 11:19:12 -0400

    updated indexview and detailview for better pub date handling

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 # from django.template import loader
 
 from .models import Choice, Question
@@ -14,11 +15,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """returns the last 5 q's"""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
