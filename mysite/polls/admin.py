288bce41c5c688db4ef7f8d294a29247c783c598
Katie Vogel <kvog983@gmail.com>
2023-06-11 12:42:00 -0400

    updates to admin to change layout, control

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,29 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-# Register your models here.
+# Basic registration of your models here.
+# admin.site.register(Question)
+# admin.site.register(Choice)
 
-admin.site.register(Question)
-admin.site.register(Choice)
+#Customizing the admin form to control how it looks and works
+
+# Reorders the page so pub date is at the top
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text'] 
+
+# Separates into sections on the django admin page, and formats it neatly
+class ChoiceInline(admin.StackedInline): #admin.TabularInline will make it a tablex
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'],
+        'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+
+admin.site.register(Question, QuestionAdmin)
